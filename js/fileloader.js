547f71edb5113fa98be13b7b9729cfdb8a701513
kumankoishi.py <7iggif@naver.com>
2021-04-28 11:34:36 +0900

    Implementing playing local video files

--- js/fileloader.js
+++ js/fileloader.js
@@ -2,5 +2,11 @@ class FileLoader {
   constructor(element, video) {
     this.element = element;
     this.video = video;
+
+    this.element.addEventListener('change', (ev) => {
+      const selected = URL.createObjectURL(this.element.files[0]);
+      console.log(selected);
+      this.video.src = selected;
+    });
   }
 }
