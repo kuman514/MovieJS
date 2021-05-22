cb58695160e3cb77f8a52e0d178290c523dbde2d
kumankoishi.py <7iggif@naver.com>
2021-05-22 15:41:36 +0900

    Adding local <-> youtube convert

--- js/app.js
+++ js/app.js
@@ -1,18 +1,35 @@
 class App {
   constructor() {
-    // Get a file loader
+    this.localVideo = document.querySelector('video');
+    this.ytVideo = document.querySelector('iframe');
+
+    // Mode manager
+    this.modeManager = new ModeManager(
+      document.getElementById('change-mode'),
+      document.querySelector('.local-video-mode'),
+      this.localVideo,
+      document.querySelector('.youtube-mode'),
+      this.ytVideo
+    );
+
+    // Local file loader
     this.fileLoader = new FileLoader(
       document.getElementById('load-file'),
       document.querySelector('video')
     );
+    // Local loop control
     this.loopControl = new LoopButton(
       document.getElementById('repeat'),
       document.querySelector('video')
     );
+
+    // YouTube URL reader
+
+    // Common mirror
     this.mirror = new Mirror(
       document.getElementById('mirror-x'),
       document.getElementById('mirror-y'),
-      document.querySelector('video')
+      document.querySelector('.video-area')
     );
   }
 }
---
+++ js/modemanager.js
@@ -0,0 +1,40 @@
+class ModeManager {
+  constructor(changeBtn, localPanel, localVideo, ytPanel, ytVideo) {
+    // mode status
+    this.mode = 'local';
+    this.changeBtn = changeBtn;
+
+    // local elements
+    this.localPanel = localPanel;
+    this.localVideo = localVideo;
+
+    // youtube elements
+    this.ytPanel = ytPanel;
+    this.ytVideo = ytVideo;
+
+    this.changeBtn.addEventListener('click', () => {
+      switch (this.mode) {
+        case 'local':
+          // convert local -> youtube
+          this.localPanel.classList.add('hidden');
+          this.localVideo.classList.add('hidden');
+          this.ytPanel.classList.remove('hidden');
+          this.ytVideo.classList.remove('hidden');
+          this.changeBtn.textContent = '';
+          this.changeBtn.appendChild(document.createTextNode('로컬 모드로 전환'));
+          this.mode = 'youtube';
+          break;
+        case 'youtube':
+          // convert youtube -> local
+          this.ytPanel.classList.add('hidden');
+          this.ytVideo.classList.add('hidden');
+          this.localPanel.classList.remove('hidden');
+          this.localVideo.classList.remove('hidden');
+          this.changeBtn.textContent = '';
+          this.changeBtn.appendChild(document.createTextNode('유튜브 모드로 전환'));
+          this.mode = 'local';
+          break;
+      }
+    });
+  }
+}
