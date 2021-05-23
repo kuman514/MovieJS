4da97769ae721d6de7fe521df9534b84aad8a3a0
kumankoishi.py <7iggif@naver.com>
2021-05-23 12:26:43 +0900

    Make YTLoader get link or id

--- js/ytloader.js
+++ js/ytloader.js
@@ -4,8 +4,23 @@ class YTLoader {
     this.loadButton = loadButton;
     this.video = video;
 
-    this.loadButton.addEventListener(() => {
-      // Parse youtube link first
-    });
+    this.loadURL = () => {
+      // Get youtube ID directly
+      if (this.urlInput.value && this.urlInput.value.length === 11) {
+        const id = this.urlInput.value;
+        this.video.src = `https://www.youtube.com/embed/${id}?autoplay=1`;
+      }
+
+      // Or parse youtube link
+      const regex = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+      const match = this.urlInput.value.match(regex);
+      console.log(match[7]);
+      if (match && match[7].length === 11) {
+        const id = match[7];
+        this.video.src = `https://www.youtube.com/embed/${id}?autoplay=1`;
+      }
+    }
+
+    this.loadButton.addEventListener('click', this.loadURL);
   }
 }
